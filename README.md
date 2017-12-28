e51b686e9021f82b74db27039107be78c002b0ac
Scott Anderson <sra@diem.net>
2017-12-28 13:48:16 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Roachize
+
+## Usage
+
+```
+$ pg_dump -h postgreshost -f mydb.sql mydb
+$ perl roachize.pl --source mydb.sql --dest mydb_for_cockroach.sql
+$ psql -h cockroachhost ... -d mydb < mydb_for_cockroach.sql
+```
+
+This is a HACK script to take a SQL (default) format output of pg_dump and emit
+a modified version that can be run against a cockroachdb instance. It is probably
+dependent on the versions of postgres I'm using and cockroach - 9.6 and 1.1.3
+
+It depends on the structure of the SQL output from the pg_dump comand so isn't going
+to work on a random sql file of postgres commands.
+
+What it does:
+
+Processes the entire file looking for the primary key definitions to hoist into
+the `CREATE TABLE` expressions- pg_dump creates the tables and adds the primary
+key constraints later. Cockroachdb requires the primary key to be defined at
+table creation. It also notices the creation of foreign key constraints and
+remembers them to explicitly create indexes for them. Postgres auto-creates them
+but cockroach does not and fails creating the constraints if the indexes do not
+exist. It dumps the DDL for them as a block when it finds an explicit `CREATE
+INDEX` which means this is after all tables have been created in a pg_dump
+
+It then rewinds and starts processing the the file again, emitting the altered
+`CREATE TABLE` commands and changing any primary keys that are integers into `SERIAL`.
+
+It comments out:
+
+```
+  CREATE SEQUENCE
+  ALTER TABLE ... PRIMARY KEY statemnts as they are already defined
+  GRANT
+  REVOKE
+  COMMENT ON
+  ALTER SEQUENCE
+  SET
+  CREATE EXTENSION
+  ALTER TABLE \S+ OWNER
+  ALTER TABLE.*nextval
+  SELECT pg_catalog)/-- \1/;
+```
+It removes the following unsupported expressions:
+```
+  USING btree
+   DEFERRABLE INITIALLY DEFERRED
+   ON DELETE SET NULL;
+```
+
+### Omitting the data
+
+You can skip the `COPY` sections during debugging to speed up imports using the `--omitdata` option.
---
+++ roachize.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/env perl
+use strict;
+use Getopt::Long 'HelpMessage', qw(:config bundling);
+
+GetOptions ('source=s'  => \my $sourceDumpFile,
+            'dest=s'    => \my $destDumpFile,
+            'omitdata'  => \my $omitData,
+            'help'     =>   sub { HelpMessage(0) },
+) or HelpMessage(1);
+
+HelpMessage(1) unless $sourceDumpFile && $destDumpFile;
+
+my $haveDumpedFKIndicies = 0;
+my %pk;
+my %pkc;
+my @fk;
+
+# First find the primary key definitions and primary key column names for serial conversion
+# and save them
+# Find foreign key constraints and collect them so we can add required indexes
+open(F, "<$sourceDumpFile") || die;
+while(<F>) {
+    if (/^ALTER TABLE ONLY (\S+)/ .. /^\s*$/) {
+        my $tablename = $1;
+        if (/\s+ADD CONSTRAINT (\S+)_pkey (PRIMARY KEY .*);/) {
+            my $pkdef = $2;
+            $pk{$tablename} = $pkdef;
+            if ($pkdef =~ /\(([a-z0-9_]+)\)/) {
+                $pkc{$tablename} = $1;
+            }
+        }
+        if (/\s+ADD CONSTRAINT (\S+) FOREIGN KEY \((\S+)\)/) {
+            push @fk, "CREATE INDEX i_${tablename}_$1 ON $tablename ($2);";
+        }
+    }
+
+    #ALTER TABLE ONLY alert
+    #ADD CONSTRAINT a_customer_fk FOREIGN KEY (customer_id) REFERENCES customer(customer_id) ;
+
+}
+close(F);
+
+
+open(F, "<$sourceDumpFile") || die;
+open(D, ">$destDumpFile") || die;
+my $curtable;
+while(<F>) {
+    # remove copy tables if $omitData is true
+    if (/^COPY / .. /^\\\./) {
+        $_ = "" if $omitData;
+        next;
+    } else {
+      $_ = cleanUnsupportedKeywords($_);
+    }
+
+    if (/^CREATE SEQUENCE/ .. /^\s*$/) {
+        $_ = "-- $_";
+    }
+
+    # In a create table we check the table name to see if we have a PK stanza saved to print 
+    # at the end
+    if (my $num = /^CREATE TABLE (\S+)/ .. /^\)/) {
+        if ($num == 1) {
+            $curtable = $1;
+        }
+        # if we are on the column that will be the pk, change the type to serial
+        if ($pkc{$curtable} && /^\s+$pkc{$curtable} integer/) {
+            s/integer/serial/;
+        }
+        if ($num =~ /E0/ && $pk{$curtable}) {
+            print D ", $pk{$curtable}\n";
+            $curtable = '';
+        }
+    }
+
+    # if we hit a create index, dump out the added ones for FKs we collcted in first pass
+    if (/^CREATE INDEX/ && !$haveDumpedFKIndicies ) {
+        $haveDumpedFKIndicies = 1;
+        print D join("\n", @fk), "\n";
+    }
+
+    # If this is a PRIMARY KEY addition, we comment it out otherwise we leave it alone
+    if (/^ALTER TABLE ONLY (\S+)/) {
+        my $lookahead = <F>;
+        $lookahead = cleanUnsupportedKeywords($lookahead);
+        if ($lookahead =~ /PRIMARY KEY/) {
+	  print D "-- $_";
+	  print D "-- $lookahead";
+	} else {
+          print D $_;
+          print D $lookahead;
+	}
+        $_ = "";
+    }
+
+} continue {
+    print D;
+}
+close(F);
+close(D);
+
+
+sub cleanUnsupportedKeywords {
+    my($f) = @_;
+    $f =~ s/^(GRANT|REVOKE|COMMENT ON|ALTER SEQUENCE|SET |CREATE EXTENSION|ALTER TABLE \S+ OWNER |ALTER TABLE.*nextval|SELECT pg_catalog)/-- \1/;
+    $f =~ s/USING btree //;
+    $f =~ s/DEFERRABLE INITIALLY DEFERRED//;
+    $f =~ s/ON DELETE SET NULL//;
+    return $f;
+}
+
+
+#ERROR:  expected 17 values, got 16
